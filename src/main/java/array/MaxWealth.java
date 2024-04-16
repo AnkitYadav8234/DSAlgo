fc18d2083ca5ab171e03845f5fde628f57abe165
AnkitYadav8234 <140303644+AnkitYadav8234@users.noreply.github.com>
2024-04-16 16:20:39 +0530

    Wealthiest person Problem

--- src/main/java/array/MaxWealth.java
+++ src/main/java/array/MaxWealth.java
@@ -1,4 +1,22 @@
 package array;
 
 public class MaxWealth {
+    public static void main(String[] args) {
+        int[][] arr = {{1,5},{7,3},{3,5}};
+        int[][] arr1 = {{2,8,7},{7,1,3},{1,9,5}};
+        System.out.println(maximumWealth(arr));
+    }
+    static int maximumWealth(int[][] accounts){
+        int ans =  Integer.MIN_VALUE;
+        for (int[] ints : accounts) {
+            int sum = 0;
+            for (int anInt : ints) {
+                sum = sum + anInt;
+            }
+            if (sum > ans){
+                ans = sum;
+            }
+        }
+        return ans;
+    }
 }
